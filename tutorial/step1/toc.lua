f116d8df624fe07364aa538b2c847bd7b889aaa9
odevices <clarkson@orthogonaldevices.com>
2021-03-06 22:21:42 +0900

    Remove the unused keyword field from the toc.lua.

--- tutorial/step1/toc.lua
+++ tutorial/step1/toc.lua
@@ -2,7 +2,6 @@ return {
   title = "Tutorial",
   author = "O|D",
   name = "tutorial",
-  keyword = "tutorial",
   units = {
     {
       -- Title and category control how this unit appears in the unit browser.
