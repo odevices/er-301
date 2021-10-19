d6b5f440f3b558fc1b4135bd2de05a7dc93fd1fb
odevices <clarkson@orthogonaldevices.com>
2021-10-19 10:37:10 +0900

    Improve comments on frame pool methods

--- od/AudioThread.h
+++ od/AudioThread.h
@@ -30,8 +30,11 @@ namespace od
     static FifoProbe *getFifoProbe();
     static void releaseFifoProbe(FifoProbe *probe);
 
+    // Returns (i.e. allocates) a FRAMELENGTH buffer from the pre-allocated pool of frame buffers.
     static float *getFrame();
+    // Releases (i.e. frees) the previously gotten frame buffer.
     static void releaseFrame(float *frame);
+    // The previous 2 calls are constant-time non-locking and thus safe to call in the audio thread.
 
 #endif
 
